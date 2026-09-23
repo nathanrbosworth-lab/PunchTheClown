f89a5e003fc6c7e50fdb422703139a429e7dd42b
nathanrbosworth-lab <nathan.r.bosworth@gmail.com>
2026-09-23 18:37:04 -0400

    M2 Beta 0.2.6 fixed 900x900 gameplay board (#10)
    * Set gameplay board to fixed 900x900 pixels
    
    * Bump to M2 Beta 0.2.6
    
    * Name 0.2.6 900px board build artifact

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rabidstudios.punchtheclown"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.2.5-m2-beta"
+        versionCode = 8
+        versionName = "0.2.6-m2-beta"
     }
 
     signingConfigs {
