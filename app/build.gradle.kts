2c02022b1c306083ab4bb1fe2350a4b2cb8391ea
nathanrbosworth-lab <nathan.r.bosworth@gmail.com>
2026-09-23 11:37:26 -0400

    Bump M2 Beta full-clown fix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rabidstudios.punchtheclown"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.3-m2-beta"
+        versionCode = 6
+        versionName = "0.2.4-m2-beta"
     }
 
     signingConfigs {
