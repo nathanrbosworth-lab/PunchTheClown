9a459998898d7404838bfd4e0a79273f3d4bc15a
nathanrbosworth-lab <nathan.r.bosworth@gmail.com>
2026-09-23 21:13:46 -0400

    M2 Beta 0.2.12 fix high score trumpet sequencing (#16)
    * Sequence high score trumpet after failure sound
    
    * Bump to M2 Beta 0.2.12
    
    * Name 0.2.12 high score audio artifact

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rabidstudios.punchtheclown"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.2.11-m2-beta"
+        versionCode = 14
+        versionName = "0.2.12-m2-beta"
     }
 
     signingConfigs {
