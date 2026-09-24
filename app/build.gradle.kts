427a971154a42a2b6b30b54b80e96fb5b6158dbc
nathanrbosworth-lab <nathan.r.bosworth@gmail.com>
2026-09-23 20:37:40 -0400

    M2 Beta 0.2.9 carnival game select screen (#13)
    * Add carnival game select artwork
    
    * Add carnival game select and tune title HUD positions
    
    * Bump to M2 Beta 0.2.9
    
    * Materialize and verify carnival select artwork

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rabidstudios.punchtheclown"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.2.8-m2-beta"
+        versionCode = 11
+        versionName = "0.2.9-m2-beta"
     }
 
     signingConfigs {
