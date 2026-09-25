bb142b639f5d49cf1cd843f7d21619b0c8a92e5c
nathanrbosworth-lab <nathan.r.bosworth@gmail.com>
2026-09-25 19:33:30 -0400

    M3.3: bump versionCode to 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rabidstudios.punchtheclown"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "0.3.2-m3-alpha"
+        versionCode = 30
+        versionName = "0.3.3-m3-alpha"
     }
 
     signingConfigs {
